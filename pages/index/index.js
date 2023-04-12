691340a4eada6d0c145cd5876aed2c8b6a90b722
HEX9CF <8148862+HEX9CF@user.noreply.gitee.com>
2023-04-12 12:25:43 +0800

    Initial Commit

---
+++ pages/index/index.js
@@ -0,0 +1,84 @@
+// pages/index/index.js
+Page({
+
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        motto: `
+            <p style="text-align: center">
+                <h1>Aspiration<br>Knowledge<br>Perseverance<br>Achievement</h1>
+                <h2>有志 有识 有恒 有为</h2>
+            </p>
+        `,
+        hr:`<br><hr><br>`,
+        campusIntro: `
+        <h3>校区介绍</h3><br>
+<p style="white-space: normal;"><span style="font-family: 仿宋_gb2312, fangsong_gb2312; font-size: 14px;">&nbsp;&nbsp;&nbsp;&nbsp;2018年12月，汕头市委明确在华侨试验区规划建设汕头大学东校区，项目选址于东海岸新城塔岗围片区东北角（翠峰路东侧），总投资约48亿元。2019年7月，在省市各级领导的关怀指导和大力支持下，汕头大学东校区正式启动建设。2022年7月，汕头大学东校区正式命名为“汕头大学东海岸校区”。2022年10月1日，汕头大学东海岸校区正式启用。2022年10月7日，汕头大学2022级4500余名新生首批入驻校区。<br></span></p>
+<p style="white-space: normal;"><span style="font-family: 仿宋_gb2312, fangsong_gb2312; font-size: 14px;">&nbsp;&nbsp;&nbsp;&nbsp;东海岸校区占地共约1571亩，其中“一场两馆”（即体育场、体育馆和训练馆）、宿舍区、教学实验行政区共占地约1088亩，可满足1万人教学实验及科研、办公需求；另有扩容新增建设用地面积约483亩，建成后可再满足1万人办学设施需求。</span></p>
+<p style="white-space: normal;"><span style="font-family: 仿宋_gb2312, fangsong_gb2312; font-size: 14px;">&nbsp;&nbsp;&nbsp;&nbsp;目前，宿舍区及教学实验行政区的主校门、教学楼已先交付汕头大学办学使用。体育场馆的体育场、体育馆及会议中心等配套设施正在完善中。教学实验行政区的图书馆及学习中心、多功能文化服务中心、行政办公楼、实验院系组团正在工程建设中，计划于2023年4月底竣工。</span></p>
+<p style="white-space: normal;"><span style="font-family: 仿宋_gb2312, fangsong_gb2312; font-size: 14px;">&nbsp;&nbsp;&nbsp;&nbsp;根据学校发展定位，东海岸校区将以交叉学科、新兴工科和应用型人文社科为主体，直接对接区域经济社会发展需求，未来致力于推动学校多校区协同发展，助力高水平大学建设。</span></p>
+<p style="text-align: center"><br></p>
+        `,
+        footer: `
+        <p style="text-align: center">Powered by HEX9CF</p>
+        <p style="text-align: center">联系QQ：919586570</p>
+        `
+    },
+
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad(options) {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady() {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow() {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide() {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload() {
+
+    },
+
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh() {
+
+    },
+
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom() {
+
+    },
+
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage() {
+
+    }
+})
---
+++ pages/map/map.js
@@ -0,0 +1,65 @@
+Page({
+  data: {
+    title: `
+    <br>
+      <p style="text-align: center">
+        <h3>东海岸校区 校园地图</h3>
+      </p>
+    <br>
+    `,
+    hr:`<br><hr><br>`,
+    latitude: 23.413634,
+    longitude: 116.837799,
+    markers: [{
+      id: 1,
+      latitude: 23.413634,
+      longitude: 116.837799,
+      name: '汕头大学(东海岸校区)'
+    }],
+    covers: [{
+      latitude: 23.411702,
+      longitude: 116.837567,
+      iconPath: '/image/location.png'
+    }]
+  },
+  onReady: function (e) {
+    this.mapCtx = wx.createMapContext('myMap')
+  },
+  getCenterLocation: function () {
+    this.mapCtx.getCenterLocation({
+      success: function(res){
+        console.log(res.longitude)
+        console.log(res.latitude)
+      }
+    })
+  },
+  moveToLocation: function () {
+    this.mapCtx.moveToLocation()
+  },
+  translateMarker: function() {
+    this.mapCtx.translateMarker({
+      markerId: 1,
+      autoRotate: true,
+      duration: 1000,
+      destination: {
+        latitude: 23.413634,
+        longitude: 116.837799,
+      },
+      animationEnd() {
+        console.log('animation end')
+      }
+    })
+  },
+  includePoints: function() {
+    this.mapCtx.includePoints({
+      padding: [10],
+      points: [{
+        latitude: 23.413634,
+        longitude: 116.837799,
+      }, {
+      latitude: 23.411702,
+      longitude: 116.837567,
+      }]
+    })
+  }
+})
